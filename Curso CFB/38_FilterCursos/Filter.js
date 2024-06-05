2a081cea424230e326245298b24826f1de243257
Santtana_25 <jhonndev25@gmail.com>
2024-06-04 21:39:18 -0300

    ++Execultando o Exercicio 038

--- Curso CFB/38_FilterCursos/Filter.js
+++ Curso CFB/38_FilterCursos/Filter.js
@@ -6,8 +6,8 @@ const btnCursoSelecionado = document.getElementById("btnCursoSelecionado")
 
 cursos.map((elem, chave) => {
     const novoElemento = document.createElement("div");
-    novoElemento.setAtribute("id", "c" + chave);
-    novoElemento.setAtribute("class", "curso c1");
+    novoElemento.setAttribute("id", `c ${chave}`);
+    novoElemento.setAttribute("class", "curso c1");
     novoElemento.innerHTML = elem;
 
 
@@ -23,4 +23,17 @@ cursos.map((elem, chave) => {
     novoElemento.appendChild(comandos);
 
     caixaCursos.appendChild(novoElemento);
+})
+
+btnCursoSelecionado.addEventListener("click", (Event) => {
+    const todosRadios = [...document.querySelectorAll("input[type=radio]")];
+
+    // Vamos filtrar
+    let radioSelecionado = todosRadios.filter((ele, ind, arr) => {
+        return ele.checked
+    })
+    radioSelecionado = radioSelecionado[0]
+    const cursoSelecionado = radioSelecionado.parentNode.parentNode
+
+    console.log(radioSelecionado)
 })
